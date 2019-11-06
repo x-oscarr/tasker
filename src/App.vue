2d92999407f99a91d7129d7b09da3869bc3d5f3a
Oscarr <insane.yan@gmail.com>
2019-11-06 17:58:21 +0200

    Create project | Add styles | New components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <TaskList
+          v-bind:tasks="tasks"
+          v-bind:title="title"
+      />
+      <div class="ocean">
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TaskList from "./components/TaskList";
 
 export default {
+
   name: 'app',
+  data() {
+      return {
+          title: 'Web developing',
+          tasks: [
+              { id: 1, text: 'Learn Vue JS', completed: false },
+              { id: 2, text: 'Learn Vue-CLI', completed: false },
+              { id: 3, text: 'Learn Node JS', completed: false },
+              { id: 4, text: 'Learn React', completed: false }
+          ]
+      }
+  },
   components: {
-    HelloWorld
+    TaskList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body { height: 100%; }
+  body {
+    background:radial-gradient(ellipse at center, rgb(255, 254, 255) 0%, rgba(255,254,255,1) 35%, #e8ebeb 100%);
+    overflow: hidden;
+  }
+
+  .ocean {
+    height: 5%;
+    width:100%;
+    position:absolute;
+    bottom:0;
+    left:0;
+    background: #015871;
+  }
+
+  .wave {
+    background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x;
+    position: absolute;
+    top: -198px;
+    width: 6400px;
+    height: 198px;
+    animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
+    transform: translate3d(0, 0, 0);
+  }
+
+  .wave:nth-of-type(2) {
+    top: -175px;
+    animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
+    opacity: 1;
+  }
+
+  @keyframes wave {
+    0% {
+      margin-left: 0;
+    }
+    100% {
+      margin-left: -1600px;
+    }
+  }
+
+  @keyframes swell {
+    0%, 100% {
+      transform: translate3d(0,-25px,0);
+    }
+    50% {
+      transform: translate3d(0,5px,0);
+    }
+  }
 </style>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,126 @@
+<template>
+    <label class="todo">
+        <input class="todo__state" type="checkbox" />
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25" class="todo__icon">
+            <use xlink:href="#todo__line" class="todo__line"></use>
+            <use xlink:href="#todo__box" class="todo__box"></use>
+            <use xlink:href="#todo__check" class="todo__check"></use>
+            <use xlink:href="#todo__circle" class="todo__circle"></use>
+        </svg>
+        <div class="todo__text">{{ task.text }}</div>
+
+    </label>
+</template>
+
+<script>
+    export default {
+        name: "TaskItem",
+        props: {
+            task: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .todo {
+        display: block;
+        position: relative;
+        padding: 1em 1em 1em 16%;
+        margin: 0 auto;
+        cursor: pointer;
+        border-bottom: solid 1px #ddd;
+    }
+    .todo:last-child {
+        border-bottom: none;
+    }
+    .todo__state {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+    }
+    .todo__text {
+        color: #135156;
+        transition: all 0.4s linear 0.4s;
+    }
+    .todo__icon {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: auto;
+        margin: auto;
+        fill: none;
+        stroke: #27FDC7;
+        stroke-width: 2;
+        stroke-linejoin: round;
+        stroke-linecap: round;
+    }
+    .todo__line,
+    .todo__box,
+    .todo__check {
+        transition: stroke-dashoffset 0.8s cubic-bezier(0.9, 0, 0.5, 1);
+    }
+    .todo__circle {
+        stroke: #27FDC7;
+        stroke-dasharray: 1 6;
+        stroke-width: 0;
+        transform-origin: 13.5px 12.5px;
+        transform: scale(0.4) rotate(0deg);
+        animation: none 0.8s linear;
+    }
+    @keyframes explode {
+        30% {
+            stroke-width: 3;
+            stroke-opacity: 1;
+            transform: scale(0.8) rotate(40deg);
+        }
+        100% {
+            stroke-width: 0;
+            stroke-opacity: 0;
+            transform: scale(1.1) rotate(60deg);
+        }
+    }
+    .todo__box {
+        stroke-dasharray: 56.1053, 56.1053;
+        stroke-dashoffset: 0;
+        transition-delay: 0.16000000000000003s;
+    }
+    .todo__check {
+        stroke: #27FDC7;
+        stroke-dasharray: 9.8995, 9.8995;
+        stroke-dashoffset: 9.8995;
+        transition-duration: 0.32000000000000006s;
+    }
+    .todo__line {
+        stroke-dasharray: 168, 1684;
+        stroke-dashoffset: 168;
+    }
+    .todo__circle {
+        animation-delay: 0.5599999999999999s;
+        animation-duration: 0.5599999999999999s;
+    }
+    .todo__state:checked ~ .todo__text {
+        transition-delay: 0s;
+        color: #5EBEC1;
+        opacity: 0.6;
+    }
+    .todo__state:checked ~ .todo__icon .todo__box {
+        stroke-dashoffset: 56.1053;
+        transition-delay: 0s;
+    }
+    .todo__state:checked ~ .todo__icon .todo__line {
+        stroke-dashoffset: -8;
+    }
+    .todo__state:checked ~ .todo__icon .todo__check {
+        stroke-dashoffset: 0;
+        transition-delay: 0.48s;
+    }
+    .todo__state:checked ~ .todo__icon .todo__circle {
+        animation-name: explode;
+    }
+</style>
